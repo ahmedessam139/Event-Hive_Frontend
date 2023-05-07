c0be088daffb6a3b432fe0ddf0eed8536039c4d0
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-05-07 20:34:27 +0300

    Complete Event Dashboard

---
+++ src/components/Admin_Components/Statistics_page_partials/AttendeesPopup.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { TextField } from '@mui/material';
+import { FaSearchengin, FaCircle, FaCheckCircle ,FaFileExcel } from "react-icons/fa";
+import { CSVLink } from 'react-csv';
+
+function AttendeesPopup({ attendees }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const headers = [
+    { label: 'Username', key: 'username' },
+    { label: 'Name', key: 'name' },
+    { label: 'No. of Tickets', key: 'tickets' },
+    { label: 'Status', key: 'status' },
+    { label: 'Transaction ID', key: 'attendeeID' },
+    { label: 'Created At', key: 'created_at' },
+  ];
+  const csvData = attendees.map(({ id, ...rest }) => rest);
+
+  return (
+    <div>
+      <div className="bg-white p-4 m-8 rounded-lg ">
+        <div className="flex justify-between mb-2">
+          <p className="mb-2 text-3xl text-gray-500">Attendees</p>
+          <div className="flex justify-end p-2">
+            <TextField
+              label="Search Attendees"
+              sx={TextFieldStyle}
+              variant="outlined"
+              type="text"
+              value={searchTerm}
+              placeholder="Search by Attendee Name"
+              InputProps={{
+                endAdornment: <FaSearchengin size={24} />,
+              }}
+            />
+            
+            <CSVLink
+              data={csvData}
+              headers={headers}
+              filename={'attendees.csv'}
+              className="ml-2"
+              target="_blank"
+            >
+            
+               <FaFileExcel size={53} className="text-green-800" />
+            </CSVLink>
+          </div>
+        </div>
+
+        <div className="bg-white shadow-md rounded my-6 overflow-x-auto">
+          <table className="min-w-max w-full table-auto">
+            <thead>
+              <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                <th className="py-3 px-6 text-center">Username</th>
+                <th className="py-3 px-6 text-center">Name</th>
+                <th className="py-3 px-6 text-center">No. of Tickets</th>
+                <th className="py-3 px-6 text-left">Status</th>
+                <th className="py-3 px-6 text-center">Transaction ID</th>
+                <th className="py-3 px-6 text-center">Created At</th>
+              </tr>
+            </thead>
+
+            <tbody className="text-gray-600 text-sm font-light">
+              {attendees.map((attendee) => (
+                <tr key={attendee.id} className="border-b border-gray-200 hover:bg-gray-100">
+                  <td className="py-3 px-6 text-center">{attendee.username}</td>
+                  <td className="py-3 px-6 text-center">{attendee.name}</td>
+                  <td className="py-3 px-6 text-center">{attendee.tickets}</td>
+                  <td className="py-3 px-6 text-center">
+                    {attendee.status === 'pending' && (
+                      <div className="flex items-center ">
+                        <FaCircle size={16} className="text-yellow-500 mr-2" />
+                        <span className="text-yellow-500 font-bold text-center">Pending</span>
+                      </div>
+                    )}
+                    {attendee.status === 'paid' && (
+                                            <div className="flex items-center ">
+                                                <FaCheckCircle size={16} className="text-green-500 mr-2" />
+                                                <span className="text-green-500 font-bold text-center">Paid</span>
+                                            </div>
+                                        )}
+                                        {attendee.status === 'attended' && (
+                                            <div className="flex items-center r">
+                                                <FaCheckCircle size={16} className="text-blue-500 mr-1" />
+                                                <span className="text-blue-500 font-semibold text-center">Attended</span>
+                                            </div>
+                                        )}
+                                    </td>
+                                    <td
+                                        className="py-3 px-6 text-center cursor-pointer "
+                                        onClick={() => handleTransactionClick(attendee.attendeeID)}
+                                    >
+                                        <span className="text-gray-500 font-semibold text-center">{attendee.attendeeID}</span>
+                                    </td>
+                                    <td className="py-3 px-6 text-center cursor-pointer ">
+                                        <span className="mr-1 text-center">{attendee.created_at}</span>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+const TextFieldStyle =
+{
+    "& .MuiOutlinedInput-root": {
+
+        "&.Mui-focused fieldset": { borderColor: "var(--darker-secondary-color)" },
+    },
+    "& .MuiInputLabel-outlined.Mui-focused": {
+        color: "var(--gray-color)"
+    }
+}
+
+export default AttendeesPopup;
--- src/components/Admin_Components/Statistics_page_partials/AttendeesTable.js
+++ src/components/Admin_Components/Statistics_page_partials/AttendeesTable.js
@@ -1,16 +1,29 @@
 import { useState } from 'react';
-import { FaCircle, FaCheckCircle } from 'react-icons/fa';
-import IoPeople from 'react-icons/io5';
+import { FaCircle, FaCheckCircle, FaTimes } from 'react-icons/fa';
+import Popup from 'reactjs-popup';
+import "reactjs-popup/dist/index.css";
+
+import AttendeesPopup from './AttendeesPopup';
 
 const AttendeesTable = () => {
-    const [attendies, setAttendies] = useState([
+    const [isOpen, setIsOpen] = useState(false);
+
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closePopup = () => {
+        setIsOpen(false);
+    };
+
+    const [attendees, setattendees] = useState([
         {
             id: 1,
             username: 'john_doe',
             name: 'John Doe',
             tickets: 2,
             status: 'pending',
-            attendieId: '345343531',
+            attendeeID: '345343531',
             created_at: '2023-05-01/10:30:00',
         },
         {
@@ -19,7 +32,7 @@ const AttendeesTable = () => {
             name: 'Jane Smith',
             tickets: 3,
             status: 'paid',
-            attendieId: '35245634534',
+            attendeeID: '35245634534',
             created_at: '2023-05-02/12:45:00',
         },
         {
@@ -28,7 +41,7 @@ const AttendeesTable = () => {
             name: 'Bob Johnson',
             tickets: 1,
             status: 'attended',
-            attendieId: '2536654564',
+            attendeeID: '2536654564',
             created_at: '2023-05-03/14:20:00',
         }, {
             id: 3,
@@ -36,13 +49,30 @@ const AttendeesTable = () => {
             name: 'Bob Johnson',
             tickets: 1,
             status: 'attended',
-            attendieId: '545645486653',
+            attendeeID: '545645486653',
+            created_at: '2023-05-03/14:20:00',
+        },
+        {
+            id: 3,
+            username: 'bob_johnson',
+            name: 'Bob Johnson',
+            tickets: 1,
+            status: 'attended',
+            attendeeID: '545645486653',
+            created_at: '2023-05-03/14:20:00',
+        },{
+            id: 3,
+            username: 'bob_johnson',
+            name: 'Bob Johnson',
+            tickets: 1,
+            status: 'attended',
+            attendeeID: '545645486653',
             created_at: '2023-05-03/14:20:00',
         },
     ]);
 
-    const handleTransactionClick = (attendieId) => {
-        // do something when attendie id is clicked
+    const handleTransactionClick = (attendeeID) => {
+        // do something when attendee id is clicked
     };
 
     return (
@@ -51,9 +81,27 @@ const AttendeesTable = () => {
                 <p className="mb-2 text-3xl text-gray-500">Attendees</p>
                 <button
                     className="bg-[color:var(--darker-secondary-color)] hover:bg-[color:var(--secondary-color)] text-white font-bold py-2 px-4 rounded-full"
+                    onClick={togglePopup}
                 >
-                    See All...
+                    See All..
                 </button>
+                <Popup
+          open={isOpen}
+          onClose={closePopup}
+          modal
+          closeOnDocumentClick
+          contentStyle={{ maxWidth: "1500px", padding: "0rem", borderRadius: "20px", width: "85%" }}
+          className="center-popup"
+        >
+          <div style={{ display: "flex", justifyContent: "flex-end", marginBottom: "-28px" }}>
+            <button className="m-3 bg-[color:var(--darker-secondary-color)] hover:bg-[color:var(--secondary-color)] text-white font-bold py-2 px-2 rounded-full"
+              onClick={togglePopup}
+            >
+              <FaTimes size={24} />
+            </button>
+          </div>
+          <AttendeesPopup attendees={attendees} />       
+          </Popup>
             </div>
 
             <div className="bg-white shadow-md rounded my-6 overflow-x-auto">
@@ -70,25 +118,25 @@ const AttendeesTable = () => {
                     </thead>
 
                     <tbody className="text-gray-600 text-sm font-light">
-                        {attendies.slice(0, 5).map((attendie) => (
-                            <tr key={attendie.id} className="border-b border-gray-200 hover:bg-gray-100">
-                                <td className="py-3 px-6 text-center">{attendie.username}</td>
-                                <td className="py-3 px-6 text-center">{attendie.name}</td>
-                                <td className="py-3 px-6 text-center">{attendie.tickets}</td>
+                        {attendees.slice(0, 5).map((attendee) => (
+                            <tr key={attendee.id} className="border-b border-gray-200 hover:bg-gray-100">
+                                <td className="py-3 px-6 text-center">{attendee.username}</td>
+                                <td className="py-3 px-6 text-center">{attendee.name}</td>
+                                <td className="py-3 px-6 text-center">{attendee.tickets}</td>
                                 <td className="py-3 px-6 text-center">
-                                    {attendie.status === 'pending' && (
+                                    {attendee.status === 'pending' && (
                                         <div className="flex items-center ">
                                             <FaCircle size={16} className="text-yellow-500 mr-2" />
                                             <span className="text-yellow-500 font-bold text-center">Pending</span>
                                         </div>
                                     )}
-                                    {attendie.status === 'paid' && (
+                                    {attendee.status === 'paid' && (
                                         <div className="flex items-center ">
                                             <FaCheckCircle size={16} className="text-green-500 mr-2" />
                                             <span className="text-green-500 font-bold text-center">Paid</span>
                                         </div>
                                     )}
-                                    {attendie.status === 'attended' && (
+                                    {attendee.status === 'attended' && (
                                         <div className="flex items-center r">
                                             <FaCheckCircle size={16} className="text-blue-500 mr-1" />
                                             <span className="text-blue-500 font-semibold text-center">Attended</span>
@@ -97,12 +145,12 @@ const AttendeesTable = () => {
                                 </td>
                                 <td
                                     className="py-3 px-6 text-center cursor-pointer "
-                                    onClick={() => handleTransactionClick(attendie.attendieId)}
+                                    onClick={() => handleTransactionClick(attendee.attendeeID)}
                                 >
-                                    <span className="text-gray-500 font-semibold text-center">{attendie.attendieId}</span>
+                                    <span className="text-gray-500 font-semibold text-center">{attendee.attendeeID}</span>
                                 </td>
                                 <td className="py-3 px-6 text-center cursor-pointer ">
-                                    <span className="mr-1 text-center">{attendie.created_at}</span>
+                                    <span className="mr-1 text-center">{attendee.created_at}</span>
                                 </td>
                             </tr>
                         ))}
@@ -110,6 +158,7 @@ const AttendeesTable = () => {
 
                 </table>
             </div>
+
         </div>
     );
 };
