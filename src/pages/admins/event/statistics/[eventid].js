c467328dcf7eacf9327a337e225ced400067f75e
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-05-07 15:36:46 +0300

    Add Empty GenderChart and AttendeesTable components

--- src/pages/admins/event/statistics/[eventid].js
+++ src/pages/admins/event/statistics/[eventid].js
@@ -1,6 +1,8 @@
 import AdminNavBar from "../../../../components/Admin_Components/AdminNavBar";
 import EventData from "../../../../components/Admin_Components/Statistics_page_partials/EventData";
 import Counters from "../../../../components/Admin_Components/Statistics_page_partials/Counters";
+import GenderChart from "../../../../components/Admin_Components/Statistics_page_partials/GenderChart";
+import AttendeesTable from "../../../../components/Admin_Components/Statistics_page_partials/AttendeesTable";
 
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -29,14 +31,25 @@ const EventStatistics = () => {
 
     }, []);
 
-if(!eventData) return null;
+    if (!eventData) return null;
 
     return (
         <div className="bg-[color:var(--primary-color)]">
             <AdminNavBar />
-            <h1>Event Statistics</h1>
             <EventData eventData={eventData} />
             <Counters counters={eventData.Counters} />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+                <div className="col-span-1 md:col-span-2">
+                {/**  Attndee Table */}
+                <AttendeesTable attendees={eventData.attendees} />
+                </div>
+                <div className="col-span-1 md:col-span-1">
+                {/** Gender Chart */}
+                <GenderChart genderPercentage={eventData.genderPercentage} />
+                </div>
+            </div>
+
+
         </div>
     );
 };
