32e90bd64cc92c3cb8389cdc7ecbd26a50f8f83f
Ahmed_Essam <ahmedessam139ae@gmail.com>
2023-05-17 01:50:59 +0300

    create Proxy Middleware

--- next.config.js
+++ next.config.js
@@ -1,19 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-};
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-            {
-                protocol: "http",
-                hostname: "**",
-            },
-        ],
-    },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://34.125.23.115:8000/api/:path*', // Replace with your backend URL
+      },
+    ];
+  },
+  async serverMiddleware() {
+    const proxy = createProxyMiddleware('/api', {
+      target: 'http://34.125.23.115:8000', // Replace with your backend URL
+      changeOrigin: true,
+      secure: false,
+    });
+
+    return {
+      '/api': proxy,
+    };
+  },
 };
